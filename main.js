f2678b05b8d410a76aeeb2705a0d787a8b201449
nicoooooo-dot <nicolas@johannakimball.com>
2025-02-18 12:01:50 -0500

    Update main.js

--- main.js
+++ main.js
@@ -1105,7 +1105,7 @@ document.querySelector("#app").innerHTML = `
                 <line x1="55.1" y1="30.4" x2="100" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
                 <line x1="45.1" y1="30.4" x2="0" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
             </svg>
-            <p class="text-red-500 leading-relaxed font-light text-xl mx-auto pb-2">button maybe</p>
+            <p class="text-red-500 leading-relaxed font-light text-xl mx-auto pb-2"></p>
         </header>
         <!-- End heading -->
         <!-- row -->
@@ -1302,7 +1302,7 @@ document.querySelector("#app").innerHTML = `
                 <line x1="55.1" y1="30.4" x2="100" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
                 <line x1="45.1" y1="30.4" x2="0" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
             </svg>
-            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">button maybe</p>
+            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2"></p>
         </header>
         <!-- End heading -->
         <!-- row -->
@@ -1480,7 +1480,7 @@ Studies and designs for the Tintal Avenue, from Bosa Avenue to Alsacia Avenue, A
                 <line x1="55.1" y1="30.4" x2="100" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
                 <line x1="45.1" y1="30.4" x2="0" y2="30.4" class="stroke-primary" style="stroke-width: 2;stroke-miterlimit: 10;"></line>
             </svg>
-            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2">button maybe</p>
+            <p class="text-gray-500 leading-relaxed font-light text-xl mx-auto pb-2"></p>
         </header>
         <!-- End heading -->
         <!-- row -->
@@ -2204,7 +2204,7 @@ Effluent Treatment Sludge Hopper Discharge System Upgrade</strong> – Engineeri
             </div>
             <div class="relative z-0 col-span-2">
               <textarea name="message" rows="5"
-                class="peer block w-full appearance-none border-0 dark:text-white border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                class="peer block w-full appearance-none border-0  border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                 placeholder=" "></textarea>
               <label
                 class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Leave
