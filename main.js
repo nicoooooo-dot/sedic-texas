395fe5254e511dee2b8cd329be1ce3a0588dcaf9
nicoooooo-dot <nicolas@johannakimball.com>
2025-02-24 11:29:45 -0500

    Update main.js

--- main.js
+++ main.js
@@ -2204,14 +2204,14 @@ Effluent Treatment Sludge Hopper Discharge System Upgrade</strong> – Engineeri
             </div>
             <div class="relative z-0 col-span-2">
               <textarea name="message" rows="5"
-                class="peer block w-full appearance-none border-0  border-b border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
+                class="peer block w-full appearance-none border-0  border-b dark:text-white border-gray-500 bg-transparent py-2.5 px-0 text-sm text-gray-900 focus:border-blue-600 focus:outline-none focus:ring-0"
                 placeholder=" "></textarea>
               <label
-                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Leave
+                class="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform text-sm  text-gray-500 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">Leave
                 Message</label>
             </div>
           </div>
-          <button type="submit" class="mt-5 rounded-md bg-black px-10 py-2 text-white">Send Message</button>
+          <button type="submit" class="mt-5 rounded-md bg-black px-10 py-2  text-white">Send Message</button>
         </form>
       </div>
     </div>
